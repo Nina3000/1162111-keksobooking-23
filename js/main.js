278dd7bbf374684c5ee6e1e57553931142056f1a
Nina3000 <31375507+Nina3000@users.noreply.github.com>
2021-06-10 23:06:15 +0300

    Второе задание

---
+++ js/main.js
@@ -0,0 +1,40 @@
+//Функция, возвращающая случайное целое число из переданного диапазона включительно.
+
+function getRandomNumber(min, max){
+  if (min < 0 || max < 0) {
+    console.log('Числа должны быть положительными. Минимальное может быть нулем.');
+  } else if (min === max || (max - min) === 1) {
+    console.log('Разница между значениями чисел должна быть равной двум или больше.');
+  } else {
+    const random = Math.random();
+    const delta = (max - min);
+    const multiply = random * delta;
+    const round = Math.round(multiply);
+    const num = round + min;
+    return(num);
+  }
+}
+//vscode жалуется на вызов консоли почему-то:
+//(unexpected console statement)
+getRandomNumber(7, 17);
+
+//src https://www.joshwcomeau.com/snippets/javascript/random/
+
+//Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
+function getRandomDecimal(min, max, fix){
+  if (min < 0 || max < 0) {
+    console.log('Числа должны быть положительными. Минимальное может быть нулем.');
+  } else if (min === max || (max - min) === 1) {
+    console.log('Разница между значениями чисел должна быть равной двум или больше.');
+  } else {
+    const random = Math.random();
+    const delta = (max - min);
+    const multiply = random * delta;
+    const adding = multiply + min;
+    const fixing = adding.toFixed(fix);
+    const num = Number(fixing);
+    return(num);
+  }
+}
+getRandomDecimal(7, 17, 1);
+//https://javascript.info/number
