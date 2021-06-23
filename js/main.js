6e83ee005d6aca9c57cee88e91e71d065fd884d3
Nina3000 <31375507+Nina3000@users.noreply.github.com>
2021-06-23 20:54:36 +0300

    4.9 Больше деталей

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ const getRandomNumber = (min, max) => {
     const num = round + min;
     return(num);
   }
-}
+};
 
 getRandomNumber(7, 17);
 
@@ -47,7 +47,7 @@ const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
 const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 const CHECKINS = ['12:00', '13:00', '14:00'];
 const CHECKOUTS = ['12:00', '13:00', '14:00'];
-const IMGS = [];
+const IMGS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
 
 const SIMILAR_OFFER_COUNT = 10;
 
@@ -58,31 +58,32 @@ const createOffer = () => {
   if (randomAvatar !== 10) {
     ava = '0' + randomAvatar;
   }
-
- // return {
- //   author: {
-  //    avatar: 'img/avatars/user' + ava + '.png'
-  //  },
+  return {
+    author: {
+     avatar: 'img/avatars/user' + ava + '.png'
+   },
     offer: {
-    title: '',
-//    address: '',
-  //  price: getRandomNumber(),
-  //  type: getRandomArrayElement(TYPES),
-    //rooms: getRandomNumber(),
-   // quests: getRandomNumber(),
-   // checkin: getRandomArrayElement(CHECKINS),
-   // checkout: getRandomArrayElement(CHECKOUTS),
-  // features: [],
-   // description: '',
-   // photos: [],
- // },
- //   location: {
-   //   lat: getRandomDecimal(35.65000, 35.70000, 5),
-    //  lng: getRandomDecimal(139.70000, 139.80000, 5)
-  //}
- // }
-//}
-//}
+    title: 'Название',
+    address: getRandomDecimal(35.65000, 35.70000, 5) + ' ' + getRandomDecimal(139.70000, 139.80000, 5),
+  price: getRandomNumber(0, 20000),
+  type: getRandomArrayElement(TYPES),
+rooms: getRandomNumber(1, 3),
+    quests: getRandomNumber(1, 5),
+ checkin: getRandomArrayElement(CHECKINS),
+ checkout: getRandomArrayElement(CHECKOUTS),
+ features: FEATURES.slice(0, getRandomNumber (0, FEATURES.length - 1)),
+ description: '',
+ photos: IMGS.slice(0, getRandomNumber (0, IMGS.length - 1)),
+ },
+   location: {
+   lat: getRandomDecimal(35.65000, 35.70000, 5),
+  lng: getRandomDecimal(139.70000, 139.80000, 5)
+}
 }
 
+
+};
+
 const similarOffers = new Array(SIMILAR_OFFER_COUNT).fill(null).map(() => createOffer());
+
+console.log(similarOffers);
