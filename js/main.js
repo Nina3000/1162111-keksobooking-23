ea328ca7ee4f11e985cc7395cd17f4edd9a83b00
Nina3000 <31375507+Nina3000@users.noreply.github.com>
2021-06-23 20:18:44 +0300

    44

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 //Функция, возвращающая случайное целое число из переданного диапазона включительно.
 
-function getRandomNumber(min, max){
+const getRandomNumber = (min, max) => {
   if (min < 0 || max < 0) {
     console.log('Числа должны быть положительными. Минимальное может быть нулем.');
   } else if (min === max || (max - min) === 1) {
@@ -20,7 +20,7 @@ getRandomNumber(7, 17);
 //src https://www.joshwcomeau.com/snippets/javascript/random/
 
 //Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
-function getRandomDecimal(min, max, fix){
+const getRandomDecimal = (min, max, fix) => {
   if (min < 0 || max < 0) {
     console.log('Числа должны быть положительными. Минимальное может быть нулем.');
   } else if (min === max || (max - min) === 1) {
@@ -40,49 +40,49 @@ getRandomDecimal(7, 17, 1);
 
 
 const getRandomArrayElement = (elements) => {
-  return elements[_.getRandomNumber(0, elements.length - 1)];
+  return elements[getRandomNumber(0, elements.length - 1)];
 };
 
 const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
 const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 const CHECKINS = ['12:00', '13:00', '14:00'];
 const CHECKOUTS = ['12:00', '13:00', '14:00'];
-const URLS = []
+const IMGS = [];
 
 const SIMILAR_OFFER_COUNT = 10;
 
-const randomAvatar = getRandomNumber (0, 11);
-if (randomAvatar === 10) {
-  ava = 10;
-} else {
-  ava = '' + 0 + randomAvatar;
-}
-}
-}
+
 const createOffer = () => {
-  return {
-    author = {
-      avatar: 'img/avatars/user' + ava + '.png'
-    }
-    offer = {
-    title: 'Номер',
-    address: 'адрес',
-    price: 4000,
-    type: getRandomArrayElement(TYPES),
-    rooms: 2,
-    quests: 2,
-    checkin: getRandomArrayElement(CHECKINS),
-    checkout: getRandomArrayElement(CHECKOUTS),
-    features: getRandomArrayElement(FEATURES),
-    description: 'уютно',
-    photos: 'url',
-    location: {
-      lat: '',
-      lng: ''
-    }
+  const randomAvatar = getRandomNumber (0, 11);
+  let ava;
+  if (randomAvatar !== 10) {
+    ava = '0' + randomAvatar;
   }
-  }
-}
+
+ // return {
+ //   author: {
+  //    avatar: 'img/avatars/user' + ava + '.png'
+  //  },
+    offer: {
+    title: '',
+//    address: '',
+  //  price: getRandomNumber(),
+  //  type: getRandomArrayElement(TYPES),
+    //rooms: getRandomNumber(),
+   // quests: getRandomNumber(),
+   // checkin: getRandomArrayElement(CHECKINS),
+   // checkout: getRandomArrayElement(CHECKOUTS),
+  // features: [],
+   // description: '',
+   // photos: [],
+ // },
+ //   location: {
+   //   lat: getRandomDecimal(35.65000, 35.70000, 5),
+    //  lng: getRandomDecimal(139.70000, 139.80000, 5)
+  //}
+ // }
+//}
+//}
 }
 
 const similarOffers = new Array(SIMILAR_OFFER_COUNT).fill(null).map(() => createOffer());
