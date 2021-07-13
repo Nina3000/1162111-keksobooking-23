e2cef0a59456fecbaf38defe4975d010c5178c81
Nina3000 <31375507+Nina3000@users.noreply.github.com>
2021-07-14 00:06:49 +0300

    7 модуль

--- js/card.js
+++ js/card.js
@@ -1,11 +1,60 @@
 import {similarOffers} from './createOffers.js';
+const typeHouse = {
+  flat: 'Квартира',
+  bungalow: 'Бунгало',
+  house: 'Дом',
+  palace: 'Дворец',
+  hotel: 'Отель',
+};
 const map = document.querySelector('#map-canvas');
 const createCard = (data) => {
   const card = document.querySelector('#card').content.cloneNode(true);
 
-
   const imgSrc = card.querySelector('.popup__avatar');
   imgSrc.src = data.author.avatar;
+
+  const address = card.querySelector('.popup__text--address');
+  address.textContent = data.offer.address;
+
+
+  const title = card.querySelector('.popup__title');
+  title.textContent = data.offer.title;
+
+  const price = card.querySelector('.popup__text--price');
+  price.textContent = data.offer.price + '₽/ночь';
+
+  const capacity = card.querySelector('.popup__text--capacity');
+  capacity.textContent = data.offer.rooms + ' комнаты для ' + data.offer.guests + ' гостей';
+
+  const time = card.querySelector('.popup__text--time');
+  time.textContent = 'Заезд после ' + data.offer.checkin + ', выезд до ' + data.offer.checkout;
+
+  const description = card.querySelector('.popup__description');
+  description.textContent = data.offer.description;
+
+const photoElement = card.querySelector('.popup__photos');
+const photos = card.offer.photos;
+photos.forEach((img) => {
+  img.src = card.offer.photos[i];
+})
+
+const featureListElement = card.querySelector('.popup__features');
+const features = data.offer.features;
+const modifiers = features.map((feature) => `popup__feature--${feature}`);
+
+featureListElement.querySelectorAll('.popup__feature')
+.forEach((item) => {
+  const modifier = item.classList[1];
+
+  if (! modifiers.includes(modifier)) {
+    item.remove();
+  }
+});
+
+
+const housing = card.querySelector('.popup__type');
+housing.textContent = typeHouse[data.offer.type];
+
 return card;
 };
 map.append(createCard(similarOffers[0]));
--- js/createOffers.js
+++ js/createOffers.js
@@ -25,11 +25,11 @@ const createOffer = () => {
   price: getRandomNumber(0, 20000),
   type: getRandomArrayElement(TYPES),
 rooms: getRandomNumber(1, 3),
-    quests: getRandomNumber(1, 5),
+    guests: getRandomNumber(1, 5),
  checkin: getRandomArrayElement(CHECKINS),
  checkout: getRandomArrayElement(CHECKOUTS),
  features: FEATURES.slice(0, getRandomNumber (0, FEATURES.length - 1)),
- description: '',
+ description: 'Описание',
  photos: IMGS.slice(0, getRandomNumber (0, IMGS.length - 1)),
  },
    location: {
